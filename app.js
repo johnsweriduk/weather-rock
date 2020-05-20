f56e04290805e7d892d7b3c464c052535d60329b
John Sweriduk <john@stackandsizzle.com>
2020-05-19 22:48:57 -0700

    finished

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 $(() => {
 	const key = 'e5a1c9b4f117b606aead46c48f1cb204';
 	const protocol = window.location.href.indexOf("https://")==0?"https://":"http://";
+	const forecast = [];
+	let forecastIndex = 0;
 	const displayWeather = (url) => {
 		$.ajax({
 			url: url
@@ -11,13 +13,40 @@ $(() => {
 			if(swayAmount > 45) {
 				swayAmount = 45;
 			}
-			$('.rain').empty();
+			$('#weather-rock .rain').empty();
 			const windAnimation = 'sway' + swayAmount;
 			console.log(windAnimation);
-			$('#rock').css('animation-name', windAnimation);
+			$('#weather-rock #rock').css('animation-name', windAnimation);
 			const rain = data.rain;
 			if(typeof rain !== 'undefined') {
 				makeItRain(rain['1h'] * 25);
+				if(rain['1h'] > 0.75) {
+					$('.rain.back-row').css('display','block');
+				}
+			}
+		});
+	}
+	const displayForecast = (url) => {
+		forecast.splice(0,forecast.length);
+		$.ajax({
+			url: url
+		})
+		.done(data => {
+			let forecast5 = data.list;
+			for(let i = 0; i < 5; i++) {
+				forecast.push(forecast5[i]);
+			}
+			let swayAmount = Math.ceil(forecast[0].wind.speed / 5) * 5
+			if(swayAmount > 45) {
+				swayAmount = 45;
+			}
+			$('#weather-rock-forecast .rain').empty();
+			const windAnimation = 'sway' + swayAmount;
+			console.log(windAnimation);
+			$('#weather-rock-forecast #rock').css('animation-name', windAnimation);
+			const rain = forecast[0].rain;
+			if(typeof rain !== 'undefined') {
+				makeItRainForecast(rain['3h'] * 75);
 				if(rain['1h'] > 0.75) {
 					$('.rain.back-row').css('display','block');
 				}
@@ -28,27 +57,97 @@ $(() => {
 		navigator.geolocation.getCurrentPosition(
 			pos => {
 				let url = `${protocol}api.openweathermap.org/data/2.5/weather?q=${pos}&appid=${key}`;
+				let forecastUrl = `${protocol}api.openweathermap.org/data/2.5/forecast?q=${pos}&appid=${key}`;
 				if(pos !== '') {
 					url = `${protocol}api.openweathermap.org/data/2.5/weather?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&appid=${key}`;
+					forecastUrl = `${protocol}api.openweathermap.org/data/2.5/forecast?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&appid=${key}`;
 				}
 				displayWeather(url);
+				displayForecast(forecastUrl);
 			}
 		);
 	}
 	$('input[type="submit"]').click( () => {
 		const loc = $('input[type="text"]').val();
+		forecastIndex = 0;
+		$('#title').find('p').hide();
+		$('#title').find('p:first-child').show();
 		if(loc !== '') {
 			const url = `${protocol}api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${key}`;
+			const forecastUrl = `${protocol}api.openweathermap.org/data/2.5/forecast?q=${loc}&appid=${key}`;
 			displayWeather(url);
+			displayForecast(forecastUrl);
 		}
 	});
 	$('input[type="text"]').keyup( e => {
 		if ($('input[type="text"]').is(":focus") && (e.keyCode == 13)) {
+			forecastIndex = 0;
+			$('#title').find('p').hide();
+			$('#title').find('p:first-child').show();
 			const loc = $('input[type="text"]').val();
 			if(loc !== '') {
 				const url = `${protocol}api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${key}`;
+				const forecastUrl = `${protocol}api.openweathermap.org/data/2.5/forecast?q=${loc}&appid=${key}`;
 				displayWeather(url);
+				displayForecast(forecastUrl);
 			}
 		}
+	});
+	$('#controls .left-arrow').click( () => {
+		if(forecastIndex == 0) {
+			forecastIndex = 4;
+		} else {
+			forecastIndex--;
+		}
+		let swayAmount = Math.ceil(forecast[forecastIndex].wind.speed / 5) * 5
+		if(swayAmount > 45) {
+			swayAmount = 45;
+		}
+		$('#weather-rock-forecast .rain').empty();
+		const windAnimation = 'sway' + swayAmount;
+		console.log(windAnimation);
+		$('#weather-rock-forecast #rock').css('animation-name', windAnimation);
+		const rain = forecast[forecastIndex].rain;
+		console.log(rain);
+		if(typeof rain !== 'undefined') {
+			makeItRainForecast(rain['3h'] * 75);
+			if(rain['1h'] > 0.75) {
+				$('.rain.back-row').css('display','block');
+			}
+		} else {
+			$('#weather-rock-forecast .rain').empty();
+		}
+		$('#title').find('p').hide();
+		let showTitle = forecastIndex + 1;
+		$('#title').find('p:nth-child(' + showTitle + ')').show();
+	});
+	$('#controls .right-arrow').click( () => {
+		if(forecastIndex == 4) {
+			forecastIndex = 0;
+		} else {
+			forecastIndex++;
+		}
+		console.log(forecast[forecastIndex]);
+		let swayAmount = Math.ceil(forecast[forecastIndex].wind.speed / 5) * 5
+		if(swayAmount > 45) {
+			swayAmount = 45;
+		}
+		$('#weather-rock-forecast .rain').empty();
+		const windAnimation = 'sway' + swayAmount;
+		console.log(windAnimation);
+		$('#weather-rock-forecast #rock').css('animation-name', windAnimation);
+		const rain = forecast[forecastIndex].rain;
+		console.log(rain);
+		if(typeof rain !== 'undefined') {
+			makeItRainForecast(rain['3h'] * 75);
+			if(rain['1h'] > 0.75) {
+				$('.rain.back-row').css('display','block');
+			}
+		} else {
+			$('#weather-rock-forecast .rain').empty();
+		}
+		$('#title').find('p').hide();
+		let showTitle = forecastIndex + 1;
+		$('#title').find('p:nth-child(' + showTitle + ')').show();
 	});
 });
--- assets/js/rain.js
+++ assets/js/rain.js
@@ -1,10 +1,9 @@
-
 const makeItRain = (amount) => {
 	if(amount > 100) {
 		amount = 100;
 	}
 	//clear out everything
-	$('.rain').empty();
+	$('#weather-rock .rain').empty();
 
 	var increment = 0;
 	var drops = "";
@@ -20,7 +19,34 @@ const makeItRain = (amount) => {
 		//add in a new raindrop with various randomizations to certain CSS properties
 		drops += '<div class="drop" style="left: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
 		backDrops += '<div class="drop" style="right: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
-		$('.rain.front-row').append(drops);
-		$('.rain.back-row').append(backDrops);
+		$('#weather-rock .rain.front-row').append(drops);
+		$('#weather-rock .rain.back-row').append(backDrops);
+	}
+}
+const makeItRainForecast = (amount) => {
+	if(amount > 100) {
+		amount = 100;
+	}
+	//clear out everything
+	$('#weather-rock-forecast .rain').empty();
+	console.log('rain');
+	var increment = 0;
+	var drops = "";
+	var backDrops = "";
+	console.log(amount);
+	while (increment < amount) {
+	console.log('rain1');
+		//couple random numbers to use for various randomizations
+		//random number between 98 and 1
+		var randoHundo = (Math.floor(Math.random() * (98 - 1 + 1) + 1));
+		//random number between 5 and 2
+		var randoFiver = (Math.floor(Math.random() * (5 - 2 + 1) + 2));
+		//increment
+		increment += randoFiver;
+		//add in a new raindrop with various randomizations to certain CSS properties
+		drops += '<div class="drop" style="left: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
+		backDrops += '<div class="drop" style="right: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
+		$('#weather-rock-forecast .rain.front-row').append(drops);
+		$('#weather-rock-forecast .rain.back-row').append(backDrops);
 	}
 }
